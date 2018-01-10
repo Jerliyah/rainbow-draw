8987c95474e29ebf4a7d6ccf2eae8ac40eb6bd49
Jerliyah <jerliyahcraig@gmail.com>
2018-01-10 15:31:46 -0600

    fancy buttons and header

--- action.js
+++ action.js
@@ -1,31 +1,65 @@
-var canvas = document.querySelector('canvas');
+
 var header = document.querySelector('header');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight - header.offsetHeight;
+var slider = document.querySelector('input');
+slider.value = 5;
 
-var ctx = canvas.getContext('2d');
+var buttons = Array.from([...document.querySelectorAll('button')]);
+
+buttons.forEach( (button) => {
+    button.addEventListener('click', function() {
+        this.classList.toggle('on');
+
+        if( this.classList.contains('on') ) {
+            // Change header background to the same as button border (except basic)
+            if( this.id === 'colorize' || this.id === 'rainbow') {
+                header.style.background = window.getComputedStyle(this).borderImageSource;
+            }
+            else {
+                header.style.background = 'linear-gradient(to right, gray, white)'
+            }
+
+            // Only one button can be on (so remove class from siblings)
+            let siblings = buttons.filter( (button) => {return button != this} );
+            siblings.forEach( (sibling) => { sibling.classList.remove('on') })
+        }
+    })
+})
+
 
 // --------------------------------------------
 
-// var drawing = false;
-// var [lastX , lastY] = [0 , 0];
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight - header.offsetHeight;
+var ctx = canvas.getContext('2d');
+ctx.lineCap = 'round';
+ctx.lineJoin = 'round';
 
-// function draw(cursor) {
-//     if(!drawing) { return }
+var drawing = false;
+var [lastX , lastY] = [0 , 0];
 
-//     ctx.beginPath();
-//     ctx.moveTo( lastX , lastY );
-//     ctx.lineTo(cursor.offsetX, cursor.offsetY);
-//     ctx.stroke();
-//     [lastX , lastY] = [cursor.offsetX , cursor.offsetY];
-// }
+function draw(cursor, effect) {
+    if(!drawing) { return }
 
-// canvas.addEventListener('mousedown', (e) => { 
-//     drawing = true ;
-//     [lastX , lastY] = [e.offsetX , e.offsetY];
-// })
-// canvas.addEventListener('mousemove', draw)
-// canvas.addEventListener('mouseup', () => { drawing = false })
+    if( effect == "none") {
+        console.log("effect worked");
+    }
+
+    ctx.lineWidth = slider.value;
+
+    ctx.beginPath();
+    ctx.moveTo( lastX , lastY );
+    ctx.lineTo(cursor.offsetX, cursor.offsetY);
+    ctx.stroke();
+    [lastX , lastY] = [cursor.offsetX , cursor.offsetY];
+}
+
+canvas.addEventListener('mousedown', (e) => { 
+    drawing = true ;
+    [lastX , lastY] = [e.offsetX , e.offsetY];
+})
+canvas.addEventListener('mousemove', (e) => { draw(e, 'none') })
+canvas.addEventListener('mouseup', () => { drawing = false })
 
 
